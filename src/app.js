77a45558109678c64d29815d44f29b7861ad6637
nilsonjunior90 <nilson.junior@dcomp.ufs.br>
2020-06-26 21:23:18 -0300

    Criar rota para exclusão

--- src/app.js
+++ src/app.js
@@ -45,7 +45,18 @@ app.put("/repositories/:id", (request, response) => {
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository=>repository.id === id);
+
+  if(repositoryIndex < 0){
+    response.status(400);
+    return response.json({"error":"Repository not found"});
+  }
+
+  repositories.splice(repositoryIndex,1);
+  
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
