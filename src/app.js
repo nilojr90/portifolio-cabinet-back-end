0f66a9c29077c10b0a4802f01020b8a42eea61e3
nilsonjunior90 <nilson.junior@dcomp.ufs.br>
2020-06-26 21:22:24 -0300

    Criar rota para alteração

--- src/app.js
+++ src/app.js
@@ -25,7 +25,23 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {title, url, techs} = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository=>repository.id === id);
+
+  if(repositoryIndex < 0){
+    response.status(400);
+    return response.json({"error":"Repository not found"});
+  }
+
+  const likes = repositories[repositoryIndex].likes;
+  const repository={id, title, url, techs, likes};
+
+  repositories[repositoryIndex] = repository; 
+  
+  response.status(200);
+  return response.json(repositories[repositoryIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
